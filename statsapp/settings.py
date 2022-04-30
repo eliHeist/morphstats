f888938cf99d427b24f3a9da1046adeddfd79080
eliHeist <eliyang256@outlook.com>
2022-04-30 11:55:21 +0300

    add allowed hosts from .env in settings.py and remove unnecesarry js and css

--- statsapp/settings.py
+++ statsapp/settings.py
@@ -18,13 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yoaa42728rdoirh#dbkxuq%z_x%^4h#6@2q%_i=&)-1yrv(j@s'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
