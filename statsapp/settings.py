f3dade7c2c7f63daa5c8415a17f57dd36928ebf6
eliHeist <eliyang256@outlook.com>
2022-04-30 11:49:36 +0300

    add allowed hosts from .env in settings.py

--- statsapp/settings.py
+++ statsapp/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-yoaa42728rdoirh#dbkxuq%z_x%^4h#6@2q%_i=&)-1yrv(j@s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition
